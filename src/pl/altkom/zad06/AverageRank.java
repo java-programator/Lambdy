8dc949120a970a516a9ca802202f11e13317b1a7
gupikuba <gupikuba@gmail.com>
2019-11-14 22:54:52 +0100

    aavie

--- src/pl/altkom/zad06/AverageRank.java
+++ src/pl/altkom/zad06/AverageRank.java
@@ -4,6 +4,8 @@ import pl.altkom.Writer;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 /*
 Popraw poniższy program, aby wypisał średnią (arytmetyczną) ocen wszystkich pisarzy (kobiet i mężczyzn)
@@ -12,7 +14,14 @@ Popraw poniższy program, aby wypisał średnią (arytmetyczną) ocen wszystkich
 public class AverageRank {
 
     public static double getAverageRank(List<Writer> writers) {
-        return 2.0;
+        Function<List<Writer>, Double> average = (lista->{
+            double sum = 0.0;
+            for(Writer w : lista){
+                sum += w.getRank()/lista.size();
+            }
+                return sum;
+        });
+        return average.apply(writers);
     }
     public static void main(String[] args) {
         List<Writer> writers = Writer.generateListOfWriters();

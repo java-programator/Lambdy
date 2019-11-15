063c2f83a34e403200cb7bda4567fe741936996e
gupikuba <gupikuba@gmail.com>
2019-11-15 23:42:24 +0100

    aa

--- src/pl/altkom/zad01/Primes.java
+++ src/pl/altkom/zad01/Primes.java
@@ -1,6 +1,7 @@
 package pl.altkom.zad01;
 
 import java.util.function.IntSupplier;
+import java.util.function.Predicate;
 
 /*
     Stwórz obiekt klasy IntSupplier, który będzie generował kolejne liczby pierwsze
@@ -18,10 +19,28 @@ import java.util.function.IntSupplier;
     Hint: Rozwiązanie z wykorzystaniem wyrażenia lambda jest trikowe i nieoczywiste, spróbuj inaczej
     Hint: Stwórz pomocniczy predykat, który wykorzystasz do sprawdzenia, czy liczba jest pierwsza
  */
-
+class PrimesGenerator implements IntSupplier{
+    int number = 2;
+    Predicate<Integer> isPrime = x->{
+        int i = 2;
+        while(i*i<=x){
+            if(x%i==0)
+                return false;
+            i++;
+        }
+        return true;
+    };
+    @Override
+    public int getAsInt() {
+        while(!isPrime.test(number))
+            number++;
+        int prime = number++;
+        return prime;
+    }
+}
 public class Primes {
     public static void main(String[] args) {
-        IntSupplier primesGenerator = null;
+        IntSupplier primesGenerator = new PrimesGenerator();
         System.out.println(primesGenerator.getAsInt());
         System.out.println(primesGenerator.getAsInt());
         System.out.println(primesGenerator.getAsInt());

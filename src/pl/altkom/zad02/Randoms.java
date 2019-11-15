9584f191559168a761c3ca9c7504b6e8fe1a6061
Your Name <you@example.com>
2019-11-15 16:05:31 +0100

    Solve zad02

--- src/pl/altkom/zad02/Randoms.java
+++ src/pl/altkom/zad02/Randoms.java
@@ -1,5 +1,6 @@
 package pl.altkom.zad02;
 
+import java.util.Random;
 import java.util.function.IntSupplier;
 
 /*
@@ -9,7 +10,9 @@ import java.util.function.IntSupplier;
 
 public class Randoms {
     public static void main(String[] args) {
-        IntSupplier randomGenerator = null;
+        Random random = new Random();
+        IntSupplier randomGenerator = () -> random.nextInt(14)+35;
+
         System.out.println(randomGenerator.getAsInt());
         System.out.println(randomGenerator.getAsInt());
         System.out.println(randomGenerator.getAsInt());

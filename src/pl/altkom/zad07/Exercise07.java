5ad881061c12c13126d3f8dd24ae6b4c510d7a64
Paweł Bogdan <pawel.bogdan1@gmail.com>
2019-11-16 13:02:43 +0100

    krok 3 zadania 7

--- src/pl/altkom/zad07/Exercise07.java
+++ src/pl/altkom/zad07/Exercise07.java
@@ -11,15 +11,28 @@ Popraw program, aby liczył sumę liczb pierwszych mniejszych od 100
 
 import java.util.function.Function;
 import java.util.function.IntFunction;
+import java.util.function.IntSupplier;
 
 public class Exercise07 {
+
+    private static IntSupplier fibonacciGenerator = new IntSupplier() {
+        int a = 0;
+        int b = 1;
+        @Override
+        public int getAsInt() {
+            int c = a + b;
+            b = a;
+            a = c;
+            return c;
+        }
+    };
 
     private static int fibonacci(int n) {
         if (n<=1) return n;
         else return fibonacci(n-1) + fibonacci(n-2);
     }
 
-    public static int countSum(IntFunction<Double> f) {
+    public static int countSum(IntSupplier generator, IntFunction<Double> f) {
         int sum = 0;
         for (int i = 0; fibonacci(i) <= 100; i++) {
             System.out.println(fibonacci(i));

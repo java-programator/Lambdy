3675b638308fba7efdc7e1a425290dc5eb0d3791
tw <tomaszwiszynski@gmail.com>
2019-11-16 11:13:30 +0100

    tw branch added

--- src/pl/altkom/zad02/Randoms.java
+++ src/pl/altkom/zad02/Randoms.java
@@ -6,7 +6,7 @@ import java.util.function.IntSupplier;
  Stwórz obiekt IntSupplier, który bedzie generował losowy numer buta,
  czyli liczbę całkowitą z przedziału 35-48
  */
-
+//
 public class Randoms {
     public static void main(String[] args) {
         IntSupplier randomGenerator = null;

20c3b30d2303b1e5eaadffea11127d673904c3b0
Paweł Bogdan <pawel.bogdan1@gmail.com>
2019-11-16 12:12:58 +0100

    Treść zadania 7

---
+++ src/pl/altkom/zad07/Exercise07.java
@@ -0,0 +1,13 @@
+package pl.altkom.zad07;
+
+/*
+Napisz funkcję, który policzy sumę liczb Fibonacciego mniejszych od 100.
+
+Dodaj do programu, aby liczył sumę ich kwadratów
+Dodaj do programu, aby liczył sumę ich sześcianów
+
+Popraw program, aby liczył sumę liczb pierwszych mniejszych od 100
+ */
+
+public class Exercise07 {
+}

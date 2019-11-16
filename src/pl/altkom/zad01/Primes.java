5aec7eca9dfa01a138605bf942b12e964986f86c
student <torba135711@gmail.com>
2019-11-16 10:28:31 +0100

    solve zad01

--- src/pl/altkom/zad01/Primes.java
+++ src/pl/altkom/zad01/Primes.java
@@ -1,5 +1,6 @@
 package pl.altkom.zad01;
 
+import java.util.function.IntPredicate;
 import java.util.function.IntSupplier;
 
 /*
@@ -20,16 +21,44 @@ import java.util.function.IntSupplier;
  */
 
 public class Primes {
+    private static int currentNumber =1;
     public static void main(String[] args) {
-        IntSupplier primesGenerator = null;
+
+        IntPredicate isPrime = n->{
+            if(n<=1) return false;
+            for(int i = 2; i*i <=n; i++)
+            {
+                if(n%i==0)return false;
+            }
+            return true;
+        };
+
+        IntSupplier primesGenerator = () -> {
+            do{
+                currentNumber++;
+            }while(!isPrime.test(currentNumber));
+            return currentNumber;
+        };
+
+        IntSupplier primesGenerator2 = new IntSupplier() {
+            int a;
+            @Override
+            public int getAsInt() {
+                do{
+                    a++;
+                }while (!isPrime.test(a));
+                return a;
+            }
+        };
+
         System.out.println(primesGenerator.getAsInt());
+        System.out.println(primesGenerator2.getAsInt());
         System.out.println(primesGenerator.getAsInt());
+        System.out.println(primesGenerator2.getAsInt());
         System.out.println(primesGenerator.getAsInt());
+        System.out.println(primesGenerator2.getAsInt());
         System.out.println(primesGenerator.getAsInt());
-        System.out.println(primesGenerator.getAsInt());
-        System.out.println(primesGenerator.getAsInt());
-        System.out.println(primesGenerator.getAsInt());
-        System.out.println(primesGenerator.getAsInt());
-        System.out.println(primesGenerator.getAsInt());
+        System.out.println(primesGenerator2.getAsInt());
+        System.out.println(primesGenerator2.getAsInt());
     }
 }

9ac9aa6d80084120c6e6187bdd319b5bc99e04ac
Paweł Bogdan <pawel.bogdan1@gmail.com>
2019-11-16 12:32:09 +0100

    krok 1 zadania 7

--- src/pl/altkom/zad07/Exercise07.java
+++ src/pl/altkom/zad07/Exercise07.java
@@ -10,4 +10,22 @@ Popraw program, aby liczył sumę liczb pierwszych mniejszych od 100
  */
 
 public class Exercise07 {
+
+    private static int fibonacci(int n) {
+        if (n<=1) return n;
+        else return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+    public static int countSum() {
+        int sum = 0;
+        for (int i = 0; fibonacci(i) <= 100; i++) {
+            System.out.println(fibonacci(i));
+            sum += fibonacci(i);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countSum());
+    }
 }
